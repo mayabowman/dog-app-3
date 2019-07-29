bc914a7e03fbacf40778077310ed61a57c4d74ce
Maya Bowman <mayashady1@gmail.com>
2019-07-29 14:00:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function getDogImages(breedInput) {
+  
+  fetch(`https://dog.ceo/api/breed/${breedInput}/images/random`)
+  .then(response => response.json())
+  .then(responseJson => displayResults(responseJson))
+  .catch(error => alert('Something went wrong. Try again later!'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  // debugger;
+  if (responseJson.message === "Breed not found") {
+    $('.results').append(`<h2>Breed not found. Please try again.</h2>`);
+  } else {
+  $('.results').html('');
+  $('.results').append(`<h2>Dog Pic Time!</h2>`);
+  $('.results').append(`<img src="${responseJson.message}" class="results">`);
+  $('.results').removeClass('hidden');
+  $('#dog-breed').val('');
+  }
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    let breedInput = $('#dog-breed').val();
+    getDogImages(breedInput);
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for input and submit!');
+  watchForm();
+});
